3aa609c77075f1e1d187a0f7bd894194371b64d6
maximnelipin <nelipin.maxim@yandex.ru>
2015-09-30 14:15:10 +0300

    new test

---
+++ _itinfo/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Insert title here</title>
+</head>
+
+    <body>
+    <p>This page uses frames. The current browser you are using does not support frames.</p>
+    <?php
+
+	?>
+    </body>
+ 
+</html>
